c0197f21ad38c1f61f21f9bde8b5ea8be71e0f6a
tishmalo <tishmalo99@gmail.com>
2026-07-11 14:04:05 +0300

    fix; Fix the Personal Access Tokens

---
+++ database/migrations/2026_07_11_000002_alter_personal_access_tokens_tokenable_id_to_uuid.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (!in_array(DB::getDriverName(), ['mysql', 'mariadb'], true)) {
+            return;
+        }
+
+        DB::statement('ALTER TABLE personal_access_tokens MODIFY tokenable_id CHAR(36) NOT NULL');
+    }
+
+    public function down(): void
+    {
+        if (!in_array(DB::getDriverName(), ['mysql', 'mariadb'], true)) {
+            return;
+        }
+
+        DB::statement('ALTER TABLE personal_access_tokens MODIFY tokenable_id BIGINT UNSIGNED NOT NULL');
+    }
+};
